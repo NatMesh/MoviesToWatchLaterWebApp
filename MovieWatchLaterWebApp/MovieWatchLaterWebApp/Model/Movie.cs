0d26d859ef3bbaf955879c2e1c2024ef8727c8be
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-01-25 11:29:34 -0600

    Created Movie model with properties(columns) Id, Name, Genre, and Medium.

---
+++ MovieWatchLaterWebApp/MovieWatchLaterWebApp/Model/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWatchLaterWebApp.Model
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //The name of the show, movie, or anime
+        [Required]
+        public string Name { get; set; }
+
+        //The genre either action, horror, romance, comedy, etc.
+        public string Genre { get; set; }
+
+        //Whether the item is a movie, tv show or anime
+        public string Medium { get; set; }
+    }
+}
