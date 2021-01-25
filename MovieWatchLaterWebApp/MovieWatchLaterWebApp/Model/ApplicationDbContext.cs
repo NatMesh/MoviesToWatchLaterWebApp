431a0d59df6cb6872ca45cefd21950000c6c8fb6
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-01-25 13:30:46 -0600

    Created ApplicationDbContext class so we can query and communicate to our database. Added DbContext as a service to our project under ConfigureServices. Created a successful migration to create our MovieList database and add Movie table.

---
+++ MovieWatchLaterWebApp/MovieWatchLaterWebApp/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWatchLaterWebApp.Model
+{
+    //A DbContext instance represents a session with the database and can be used to query and save instances of your entities.
+    //make our class inherit form DbContext which is a class built in EntityFrameworkCore
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
