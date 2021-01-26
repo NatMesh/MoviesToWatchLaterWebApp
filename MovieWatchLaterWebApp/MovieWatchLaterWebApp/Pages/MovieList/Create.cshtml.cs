746464617484e26db75aeff81577b626862a795d
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-01-26 12:37:38 -0600

    Implemented OnPost() method for our create route so we can now add movies/shows to our watch later list. Also added some validations to our crete form to let user know Name is required field.

--- MovieWatchLaterWebApp/MovieWatchLaterWebApp/Pages/MovieList/Create.cshtml.cs
+++ MovieWatchLaterWebApp/MovieWatchLaterWebApp/Pages/MovieList/Create.cshtml.cs
@@ -17,11 +17,30 @@ namespace MovieWatchLaterWebApp.Pages.MovieList
             _db = db;
         }
 
+        //This property sets it so on post the method OnPost() will assume we get a Movie object
+        [BindProperty]
         public Movie Movie { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                //.Add preforms an insert into our Movie Table with the passed form data
+                await _db.Movie.AddAsync(Movie);
+                //.SaveChanges() persists the data to 
+                await _db.SaveChangesAsync();
+                //Takes us back to our List of media to watch later page
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
