0379d4882b0593f0eb8291cbd8bbd8f11fe0817a
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-01-25 19:22:47 -0600

    Created a table on the index page to display all the movies/shows a user plans to watch. Added buttons for deleting, editing and adding new movies/shows to the list.

--- MovieWatchLaterWebApp/MovieWatchLaterWebApp/Pages/MovieList/Index.cshtml.cs
+++ MovieWatchLaterWebApp/MovieWatchLaterWebApp/Pages/MovieList/Index.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieWatchLaterWebApp.Model;
 
 namespace MovieWatchLaterWebApp.Pages.MovieList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            //allows us to extract the ApplicationDbContext Object that is inside the dependency container and inject it into this page.
+            _db = db;
+        }
+
+        //Create an IEnumerable to store our list of Movie records returned from our database.
+        public IEnumerable<Movie> Movies { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            //This call to our database retrieves all the Movies from our database
+            Movies = await _db.Movie.ToListAsync();
         }
     }
 }
