75034653089b81838ef4b3715926e90a7c80efd0
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-01-26 12:58:09 -0600

    Created Edit razor page that will be called when users click on the edit button. It will pass the id so the record's information can be placed in a form for easy editing.

---
+++ MovieWatchLaterWebApp/MovieWatchLaterWebApp/Pages/MovieList/Edit.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MovieWatchLaterWebApp.Model;
+
+namespace MovieWatchLaterWebApp.Pages.MovieList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Movie = await _db.Movie.FindAsync(id);
+        }
+    }
+}
